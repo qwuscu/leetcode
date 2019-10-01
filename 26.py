5f14d0a329dfd5dcea4ab0af9129c446eb66b10b
gracewu <gracewu@cisco.com>
2019-10-01 14:17:16 -0700

    update typing

--- 26.py
+++ 26.py
@@ -1,10 +1,15 @@
-class Solution:
+from typing import List
+
     #two pointers, time complexity: O(n), spcae: O(1)
-    def removeDuplicates(self, nums):
-        if not nums: return 0
-        i = 0
-        for j in range(0, len(nums)):
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0;
+        for j in range(1, len(nums)):
             if nums[i] != nums[j]:
                 i += 1
                 nums[i] = nums[j]
-        return i + 1
+        return i+1
+
+
+if __name__ == "__main__":
+    print(Solution().removeDuplicates([1, 1, 2]))
