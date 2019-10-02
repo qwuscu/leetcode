c2885825cb5dc187c4b070d8244e0f11bd8994aa
gracewu <gracewu@cisco.com>
2019-10-02 16:55:30 -0700

    create 88.py

---
+++ 88.py
@@ -0,0 +1,38 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        k = m + n - 1
+        i = m - 1
+        j = n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] <= nums2[j]:
+                nums1[k] = nums2[j]
+                j -= 1
+            else:
+                nums1[k] = nums1[i]
+                i -= 1
+            k -= 1
+        while j >=0 :
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+        return nums1
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while m > 0 and n > 0:
+            if nums2[n-1] >= nums1[m-1]:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+            else:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+        if m == 0:
+            nums1[0:n] = nums2[0:n]
+                
