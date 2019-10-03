8576a76f63f0696dc7c600d4dbf318f6c87d23e2
gracewu <gracewu@cisco.com>
2019-10-03 10:44:37 -0700

    update

--- 88.py
+++ 88.py
@@ -3,22 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        #space:  O(1), time: O(m+n)
         k = m + n - 1
-        i = m - 1
-        j = n - 1
-        while i >= 0 and j >= 0:
-            if nums1[i] <= nums2[j]:
-                nums1[k] = nums2[j]
-                j -= 1
+        m = m - 1
+        n = n - 1
+        while m >= 0 and n >= 0:
+            if nums2[n] >= nums1[m]:
+                nums1[k] = nums2[n]
+                n -= 1
             else:
-                nums1[k] = nums1[i]
-                i -= 1
+                nums1[k] = nums1[m]
+                m -= 1
             k -= 1
-        while j >=0 :
-            nums1[k] = nums2[j]
-            j -= 1
+        while n >= 0:
+            nums1[k] = nums2[n]
             k -= 1
-        return nums1
+            n -= 1
+
 
 
 class Solution:
@@ -35,4 +36,3 @@ class Solution:
                 m -= 1
         if m == 0:
             nums1[0:n] = nums2[0:n]
-                
