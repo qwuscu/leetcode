0655680c4f98a14584104c268d63bd12ab0b737d
gracewu <gracewu@cisco.com>
2019-10-04 16:02:25 -0700

    create

---
+++ 118.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        for num in range(numRows):
+            nums = [None for _ in range(num+1)]
+            nums[0], nums[-1] = 1, 1 #first and last element in the row is 1
+            for j in range(1, num):
+                 nums[j] = res[num-1][j-1] + res[num-1][j]
+            res.append(nums)
+        return res
+    #time: O(n^2)
