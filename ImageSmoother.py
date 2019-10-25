43e2ef35ffa7b34c9b63b6caea88963c52ccdd47
gracewu <gracewu@cisco.com>
2019-10-24 23:54:40 -0700

    update 661

--- ImageSmoother.py
+++ ImageSmoother.py
@@ -39,3 +39,21 @@ class Solution:
 
 print(Solution().imageSmoother([[2,3,4],[5,6,7],[8,9,10],[11,12,13],[14,15,16]]))
 # print(Solution().imageSmoother([[1,1,1],[1,0,1],[1,1,1]]))
+
+
+class Solution(object):
+    def imageSmoother(self, M):
+        R, C = len(M), len(M[0])
+        ans = [[0] * C for _ in M]
+
+        for r in xrange(R):
+            for c in xrange(C):
+                count = 0
+                for nr in (r-1, r, r+1):
+                    for nc in (c-1, c, c+1):
+                        if 0 <= nr < R and 0 <= nc < C:
+                            ans[r][c] += M[nr][nc]
+                            count += 1
+                ans[r][c] /= count
+
+        return ans
