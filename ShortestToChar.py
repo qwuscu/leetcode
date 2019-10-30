e3b46ecf2932ffc8ec41fe725629a029a1fe80ee
gracewu <gracewu@cisco.com>
2019-10-30 15:40:11 -0700

    update 821

--- ShortestToChar.py
+++ ShortestToChar.py
@@ -2,6 +2,8 @@ from typing import List
 
 
 class Solution:
+    # 将字符串按正、逆顺序两次扫描，第一次扫描计算当前位置字母与前一个目标字母之间的距离，
+    # 后一次扫描计算当前位置字母与后一个字母之间的距离，两者取小即可。
     def shortestToChar(self, S: str, C: str) -> List[int]:
         n = len(S)
         res = [n] * n
