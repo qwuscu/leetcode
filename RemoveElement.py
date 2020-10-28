bf3a78b217ad48e356f2f328722ff4f55b35a377
Grace Wu (gracewu) <gracewu@cisco.com>
2020-10-28 16:46:29 -0700

    27

--- RemoveElement.py
+++ RemoveElement.py
@@ -1,14 +1,44 @@
-from typing import List
+# from typing import List
+#
+# #two pointer, time: O(n), space: O(1)
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         i = 0
+#         j = len(nums)
+#         while i < j:
+#             if nums[i] == val:
+#                 nums[i] = nums[j-1]
+#                 j -= 1
+#             else:
+#                 i += 1
+#         return j
 
-#two pointer, time: O(n), space: O(1)
+class Solution:
+    """
+    @param: A: A list of integers
+    @param: elem: An integer
+    @return: The new length after remove
+    """
+    def removeElement(self, A, elem):
+        # write your code here
+        i = 0
+        j = 0
+        for j in range(len(A)):
+            if A[j] != elem:
+                A[i] = A[j]
+                i += 1
+        return A[:i]
+
+print(Solution().removeElement([0,4,4,0,0,2,4,4], 4))
+
+
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
         i = 0
-        j = len(nums)
-        while i < j:
-            if nums[i] == val:
-                nums[i] = nums[j-1]
-                j -= 1
-            else:
+        for j in range(n):
+            if nums[j] != val:
+                nums[i] = nums[j]
                 i += 1
-        return j
+        return i
