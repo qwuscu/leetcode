30929a2afc4c4eab0456e5f54fabfe0091ee4228
Grace Wu (gracewu) <gracewu@cisco.com>
2020-11-09 14:32:44 -0800

    88

---
+++ merge_sorted_array.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        k = m+n-1
+        m = m-1
+        n = n-1
+        while m>=0 and n>=0:
+            if nums1[m] > nums2[n]:
+                nums1[k] = nums1[m]
+                m -= 1
+            else:
+                nums1[k] = nums2[n]
+                n -= 1
+            k -= 1
+        while n >= 0:
+            nums1[k] = nums2[n]
+            k -= 1
+            n -= 1
+        return nums1
+    #space: O(1), time: O(m+n)
+
+print(Solution().merge([4,5,6,0,0,0],3,[1,2,3],3))
