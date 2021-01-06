8f8fc60a6e867365ccc241a0d0d4d3ffe8ae193f
Grace Wu (gracewu) <gracewu@cisco.com>
2021-01-06 15:37:02 -0800

    832 new methods

--- FlipInvertImage.py
+++ FlipInvertImage.py
@@ -30,4 +30,25 @@ class Solution:
                         A[i][n-j-1] = 0
         return A
     
-    
+
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        m = len(A)
+        n = len(A[0])
+        for i in range(m):
+            A[i] = A[i][::-1]
+            for j in range(n):
+                A[i][j] ^= 1
+        return A
+
+
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        for row in A:
+            for i in range((len(row) + 1) // 2):
+                """
+                In Python, the shortcut row[~i] = row[-i-1] = row[len(row) - 1 - i]
+                helps us find the i-th value of the row, counting from the right.
+                """
+                row[i], row[~i] = row[~i] ^ 1, row[i] ^ 1
+        return A
