912b8dbe9794d4a5ec99b854fef3ea3986805569
Grace Wu (gracewu) <gracewu@cisco.com>
2021-01-08 17:07:20 -0800

    442

---
+++ find_all_duplicates_in_an_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        seen = set()
+        ans = []
+        for num in nums:
+            if num in seen:
+                ans.append(num)
+            else:
+                seen.add(num)
+        return ans
