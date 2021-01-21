5068db63c379ab08c8c5ca71ab359adcb5bc8651
Grace Wu (gracewu) <gracewu@cisco.com>
2021-01-21 11:32:04 -0800

    414

--- third_maximum_number.py
+++ third_maximum_number.py
@@ -3,9 +3,19 @@ from typing import List
 
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+        nums = set(nums)
+        nums = list(nums)
+        nums.sort()
+        if len(nums) < 3: return max(nums)
+        return nums[-3]
+
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
         ans = [-float('inf')] * 3
         for num in nums:
-            # need to check ==, it may have duplicate val. just >, fail at [2,2,3,1]
+            # need to check ==, it may have duplicate val. just >, fail at [2,2,3,2]
+            # if [2,2,3,2] => ans will be [3,2,2]
             if num >= ans[0]:
                 if num == ans[0]:
                     # when =, continue. otherwise, it may goes to ans[1]
