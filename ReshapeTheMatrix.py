407dec25277b924900f123377eae7a096b18cd4d
Grace Wu (gracewu) <gracewu@cisco.com>
2021-01-31 23:45:19 -0800

    566

--- ReshapeTheMatrix.py
+++ ReshapeTheMatrix.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
         if r * c != len(nums) * len(nums[0]):
@@ -14,5 +17,26 @@ class Solution:
                     j = 0
                     newNums_row = []
         return newNums
-                
-        
+
+
+
+
+class Solution:
+    def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if r * c != len(nums) * len(nums[0]):
+            return nums
+
+        from queue import Queue
+        que = Queue()
+        for row in nums:
+            for num in row:
+                que.put(num)
+        new_nums = []
+        for i in range(r):
+            new_row = []
+            for j in range(c):
+                new_row.append(que.get())
+            new_nums.append(new_row)
+        return new_nums
+
+print(Solution().matrixReshape([[1,2],[3,4]], 1, 4))
