4262855899c79cad105a046d94d7250ecd76af7e
Grace Wu (gracewu) <gracewu@cisco.com>
2021-02-15 10:58:19 -0800

    763

---
+++ partition_labels.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        last = {c: i for i, c in enumerate(S)}
+        j = anchor = 0
+        ans = []
+        for i, c in enumerate(S):
+            j = max(j,last[c])
+            if i == j:
+                ans.append(i-anchor+1)
+                anchor = i+1
+        return ans
+
+
+print(Solution().partitionLabels("ababcbacadefegdehijhklij"))
