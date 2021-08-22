aa36cb9901114aa43d8b499361e5603942d57086
qwuscu <qwu.scu@gmail.com>
2021-08-21 23:42:49 -0700

    Update 202.py

--- 202.py
+++ 202.py
@@ -16,6 +16,25 @@ class Solution:
             known.add(num)
             n = num
         return True
+ 
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while True:
+            total_num = 0
+            while n > 0:
+                total_num += (n % 10) ** 2
+                n = n // 10
+
+            if total_num == 1:
+                return True
+            if total_num in seen:
+                return False
+            seen.add(total_num)
+
+            n = total_num
+
+        # return True
 
 if __name__ == '__main__':
     print(Solution().isHappy(19))
